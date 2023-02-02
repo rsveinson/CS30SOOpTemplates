0104d10079d75a9812b2725f3f5ca6e7722a448b
rsveinson <rsveinson@mts.net>
2023-02-02 07:18:02 -0600

    added static constants the the driver tempate for new line and delimiter

--- ClientTemplate.java
+++ ClientTemplate.java
@@ -14,55 +14,55 @@ import java.text.NumberFormat;
  *************************************************************/
 
 public class ClientTemplate {
+    // a new line character that works on every computer system
+    final static String nl = System.lineSeparator();
+    final static String delim = "[ ]+";          // delimiter for splitting input records
 
     public static void main(String[] args) throws IOException{
-    // ***** constants *******
-    
-    // ***** variables *****
-    
+        // ***** constants *******
+
+        // ***** variables *****
         String banner = "";             // output banner
         String prompt = "";             // prompt for user input
-        
+
         String strin = "";              // string fro keyboard input
         String strout = "";             // string for formatted output
-        
-        String delim = "[ ]+";          // delimiter for splitting input records
+
         String[] tokens = null;         // used to split input records
-        
+
         // a new line character that works on every computer system
-        String nl = System.lineSeparator();
-    
-    // ***** objects *****
+
+        // ***** objects *****
         ProgramInfo pInfo = new ProgramInfo("Ax Qy");
         //Scanner scanner = new Scanner(System.in);
         //NumberFormat currency = NumberFormat.getCurrencyInstance();
-        
+
         // file io buffers for reading and writing to text files
-        
+
         //BufferedReader fin = new BufferedReader(new FileReader("filename.txt"));
         //PrintWriter fout = new PrintWriter(new BufferedWriter(new FileWriter("outfle.txt")));
-    
-    // ***** print banners *****
-        
+
+        // ***** print banners *****
+
         System.out.println(pInfo.getBanner());
         //fout.print(banner);
-    
-    // ***** Get Input *****
-    
-    // ***** Main Processing *****
-    
-    // ***** Print Formatted Output *****
-    
-    // ***** Closing Message *****
-    
+
+        // ***** Get Input *****
+
+        // ***** Main Processing *****
+
+        // ***** Print Formatted Output *****
+
+        // ***** Closing Message *****
+
         System.out.println();
         System.out.println(pInfo.getClosingMessage());
         //fout.println("End of Processing");
-        
-    // **** close io buffers *****
-    
+
+        // **** close io buffers *****
+
         //fin.close();
         //fout.close();
     } // end main 
-    
+
 } // end FormatTemplate
